aae275008afc2d3675b9b7853540e81b19f2af8c
Mysticpasta1 <ethana5tc@outlook.com>
2022-07-26 05:44:36 -0500

    push for water

---
+++ src/main/java/com/mystic/aquarium/Aquarium.java
@@ -0,0 +1,10 @@
+package com.mystic.aquarium;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Aquarium implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
